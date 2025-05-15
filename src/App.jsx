26590cab552a87e6ab694338e4cd78ba114fb6f5
PactForge <pactforge@gmail.com>
2025-05-16 02:53:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ import Navbar from './components/Navbar';
 const App = () => {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen bg-gray-100">
         <Navbar />
-        <div className="container mx-auto p-4">
+        <div className="container mx-auto px-4 py-8">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
